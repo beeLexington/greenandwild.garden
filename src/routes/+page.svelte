26147c250f84062633e73e134f9b00a907b330af
Lorna <lornaharwood@protonmail.com>
2024-08-08 12:19:41 +0100

    Set up route pages

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<h1>Green and Wild</h1>
+<h2>Low impact gardening</h2>
+
+<nav> 
+    <ul>
+        <li><a href="/about">about</a></li>
+        <li><a href="/services">services</a></li>
+        <li><a href="/contact">contact</a></li>
+    </ul>
+</nav>
+
+<p>
+    Nulla anim esse sit cupidatat ullamco nisi aute. Eiusmod elit non pariatur
+    deserunt laboris minim anim deserunt pariatur incididunt excepteur proident
+    ad fugiat. Occaecat amet quis incididunt ea qui aliqua pariatur duis minim
+    mollit. Labore qui nisi officia incididunt mollit pariatur cillum anim
+    deserunt. Adipisicing irure enim dolor sit esse qui in est veniam excepteur
+    aliqua incididunt do irure. Fugiat tempor quis magna consectetur laborum
+    irure ipsum velit. In commodo incididunt adipisicing tempor ut excepteur sit
+    nostrud consequat ipsum consectetur ipsum cillum et.
+</p>
+
+<p>
+    Nostrud non eiusmod cillum quis. Eiusmod velit occaecat aliquip ad excepteur
+    voluptate. Minim consequat laborum occaecat elit nulla enim.
+</p>
+
+<p>
+    Dolor adipisicing occaecat nisi Lorem nulla est labore cupidatat. Sit ut do
+    reprehenderit ex deserunt quis ex ut ullamco. Irure ea culpa laborum
+    excepteur exercitation anim nisi enim fugiat irure ex dolor qui culpa. Id
+    aute fugiat do occaecat non incididunt. Aute voluptate labore velit anim in
+    labore eu officia laboris.
+</p>
+
+<p>
+    Laborum est nostrud commodo do sint adipisicing culpa quis pariatur.
+    Consectetur excepteur veniam velit elit esse ipsum. Aliquip eiusmod est
+    dolor aliqua culpa id culpa amet ea excepteur veniam ut. In proident et ex
+    velit amet excepteur dolore. Cillum tempor excepteur incididunt sint. Mollit
+    id ex esse ad enim officia ex.
+</p>
+
+<p>
+    Laborum dolor reprehenderit ex sunt. Culpa sunt excepteur quis incididunt
+    ullamco velit fugiat in do exercitation exercitation ut irure consequat.
+    Mollit eiusmod cillum mollit sint eu cillum irure deserunt exercitation
+    irure ea sunt cupidatat sint. Qui ut nisi culpa proident nisi proident.
+    Laborum velit occaecat laborum sunt reprehenderit culpa ullamco velit irure.
+    Anim elit exercitation laboris magna consequat voluptate fugiat pariatur
+    deserunt. Ad consequat eu officia duis reprehenderit nostrud duis labore
+    aliquip excepteur.
+</p>
