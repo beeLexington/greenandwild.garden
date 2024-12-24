5361cd3977fdc042a215955c7106cc0123c65504
Lorna <lornaharwood@protonmail.com>
2024-12-24 15:56:00 +0000

    Remove sub pages
    We are going to use a single page approach going forward

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -4,14 +4,6 @@
 
 <header>
     <Logo />
-
-    <nav>
-        <ul>
-            <li><a href="/about">about</a></li>
-            <li><a href="/services">services</a></li>
-            <li><a href="/contact">contact</a></li>
-        </ul>
-    </nav>
 </header>
 
 <style>
@@ -20,19 +12,5 @@
         display: flex;
         align-items: center;
         justify-content: space-between;
-    }
-
-    nav {
-        padding-inline-end: 1em;
-    }
-
-    nav > ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-
-        display: flex;
-        gap: 1em;
-        font-size: 1.3rem;
     }
 </style>
