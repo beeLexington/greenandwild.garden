7328c50ec79dd5182516a0953f5129c2a75f9822
Lorna <lornaharwood@protonmail.com>
2024-12-28 17:48:01 +0000

    Add in more content
    This includes contact details as well as a footer and other tweaks

---
+++ src/lib/footer.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<footer>
+    <p>
+        <a href="/privacy">Privacy policy</a>
+    </p>
+    <p>
+        <a href="/terms">Terms and conditions</a>
+    </p>
+    <p>Copyright ©2024 Green and Wild. All Rights Reserved</p>
+</footer>
+
+<style>
+    footer {
+        background-color: rgb(var(--colour-accent));
+        padding: 1rem 1.5rem;
+    }
+</style>
