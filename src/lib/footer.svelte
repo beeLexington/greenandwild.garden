214c8b84c364764b62b7fec6afaa64f9f5bf7dcd
Lorna <lornaharwood@protonmail.com>
2024-12-28 17:48:01 +0000

    WIP

---
+++ src/lib/footer.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<footer>
+    <p>this is the footer</p>
+</footer>
+
+<style>
+    footer {
+        background-color: rgb(var(--colour-accent));
+        padding: 1rem 1.5rem;
+    }
+</style>
