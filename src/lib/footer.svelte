b93535140cc32d6c6e931defa800fcca8911c8dd
erbridge <f@erbridge.co.uk>
2024-12-29 14:59:29 +0000

    Space out copyright symbol and date
    This is the usual way to express it.

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -8,7 +8,7 @@
     <p>
         <a href="/terms">Terms and conditions</a>
     </p>
-    <p>Copyright ©2024 Green and Wild. All Rights Reserved</p>
+    <p>Copyright © 2024 Green and Wild. All Rights Reserved</p>
 </footer>
 
 <style>
