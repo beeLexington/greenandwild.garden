f1ca3f9d81b686b14e40eaa181685cadf205e491
Lorna <lornaharwood@protonmail.com>
2025-01-27 11:56:18 +0000

    Turn carbon badge off
    This is just temporary whilst I sort out the contact form.
    I don't want to advertise that my website has a high carbon rating.
    Add this back in once my rating is good.

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -7,12 +7,6 @@
 		</ul>
 		<p>Copyright © 2025 Green and Wild. All Rights Reserved</p>
 	</div>
-
-	<div id="wcb" class="carbonbadge"></div>
-	<script
-		src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js"
-		defer
-	></script>
 </footer>
 
 <style>
