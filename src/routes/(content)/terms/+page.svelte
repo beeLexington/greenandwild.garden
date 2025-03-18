fbcadc49decd02c4968562c48c45cba33f7ed5af
Lorna <lornaharwood@protonmail.com>
2025-03-18 08:35:15 +0000

    Add terms of service page
    This page will only be available via url which I will use to send people
    to directly when needed.

---
+++ src/routes/(content)/terms/+page.svelte
@@ -0,0 +1,165 @@
+<section>
+    <h1>Terms of Service</h1>
+</section>
+
+<nav>
+    <ol>
+        <li><a href="#agreement">Agreement</a></li>
+        <li>
+            <a href="#consultation">Consultation</a>
+        </li>
+        <li><a href="#pricing">Pricing & fees</a></li>
+        <li>
+            <a href="#chargeable-time">Chargeable time</a>
+        </li>
+        <li><a href="#materials">Materials</a></li>
+        <li><a href="#mileage">Mileage</a></li>
+        <li><a href="#cancellation">Withdrawal & cancellation</a></li>
+    </ol>
+</nav>
+
+<section>
+    <p>Last updated: <strong>16th March 2025</strong></p>
+</section>
+
+<section id="agreement">
+    <h2>Agreement</h2>
+    <p>
+        By agreeing for me to commence work for you, you are agreeing to these
+        Terms of Service. It is your responsibility to read and understand them
+        in full before agreeing to use my services and any such agreement will
+        be considered to implicitly include an agreement to these Terms of
+        Service in full.
+    </p>
+    <p>
+        I am happy to answer any questions you might have about these Terms of
+        Service at any time.
+    </p>
+</section>
+
+<section id="consultation">
+    <h2>Consultation</h2>
+
+    <p>
+        Before agreeing to start working with you, I will usually meet with you
+        to understand and discuss your needs and expectations and to explain how
+        I will work with you. This will usually involve a site visit, during
+        which I will usually take photos and record measurements.
+    </p>
+</section>
+
+<section id="pricing">
+    <h2>Pricing & fees</h2>
+
+    <p>
+        As part of the consultation process, I will inform you of the price you
+        will pay for my services. This will either be an hourly rate or a fixed
+        fee at my discretion.
+    </p>
+    <p>I will not charge for the consultation process.</p>
+    <p>
+        Payment will be collected and due immediately at the end of each visit
+        unless I specifically and explicitly specify otherwise.
+    </p>
+    <p>
+        I accept the following payment methods: cash, credit or debit card, or
+        bank transfer.
+    </p>
+    <p>
+        I am able to send you a receipt by email or text message, and if I have
+        an email address on record for you, I will send receipts by email unless
+        you request otherwise.
+    </p>
+    <p>
+        I reserve the right to discontinue ongoing services for you with no
+        notice in the event of an inability or failure to pay for services I
+        have carried out, or any other chargeable costs associated with my work
+        for you, until all outstanding payments are made.
+    </p>
+</section>
+
+<section id="chargeable-time">
+    <h2>Chargeable time</h2>
+
+    <p>
+        Unless specifically and explicitly agreed otherwise, I charge for all of
+        the time I am on site. This includes all time spent setting up at the
+        beginning of my visit and packing up at the end. I also charge for time
+        spent doing research or making plans for your space while off site.
+    </p>
+    <p>
+        While I am on site, I will focus entirely on the services I am carrying
+        out for you, with the exception of setting up and packing up and any
+        breaks I take. If I need to take more than 5 minutes of breaks in any 1
+        hour period for any reason, I will not charge for the time in excess of
+        5 minutes.
+    </p>
+    <p>
+        If I need to go off site in order to carry out my duties for you, for
+        example to visit a garden centre to purchase materials for use in your
+        garden, I will charge for the entire duration of my time off site
+        carrying out work on your behalf, including any travel time.
+    </p>
+</section>
+
+<section id="materials">
+    <h2>Materials</h2>
+
+    <p>
+        All materials to be used while working for you must either be provided
+        by you or you must reimburse me for any costs associated with acquiring
+        them for you, including but not limited to the purchase cost of the
+        materials, VAT, and any shipping and handling fees.
+    </p>
+</section>
+
+<section id="mileage">
+    <h2>Mileage</h2>
+
+    <p>
+        I reserve the right to charge for mileage for any journey associated
+        with the work I carry out for you at the standard rate of £0.45 per
+        mile.
+    </p>
+    <p>
+        I will never charge for both mileage and travel time for the same
+        journey.
+    </p>
+</section>
+
+<section id="cancellation">
+    <h2>Withdrawal & cancellation</h2>
+
+    <p>
+        At any point during or following the consultation, but before I have
+        commenced work with you, you will have the opportunity to withdraw from
+        my services at no cost.
+    </p>
+    <p>
+        Once I have started work, you may withdraw from my services by informing
+        me in writing. You will owe me for any work I have done for you up until
+        that point. If we agreed a fixed fee and I have not completed the work
+        set out to be covered by that fee, you will owe me the lesser of the
+        fixed fee and the total time I have spent so far, charged at my standard
+        hourly rate at the time.
+    </p>
+    <p>
+        While I will make every effort to honour my agreements with you, I
+        reserve the right to discontinue my services for you at any time for any
+        reason, charging for any work already carried out or any other costs
+        already incurred by you.
+    </p>
+    <p>
+        I will endeavour to provide you with at least 48 hours notice before
+        cancelling any appointments I have with you, and expect you to do the
+        same.
+    </p>
+</section>
+
+<style>
+    nav,
+    section {
+        max-width: 60ch;
+        padding: 1rem 1.5rem;
+    }
+</style>
